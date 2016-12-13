2980f6199273ec242dd4c2c19d63337c6c639eae
lillccian <lillccian@gmail.com>
2016-12-13 22:40:20 +0800

    add api create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,21 @@ class Api::V1::UsersController < Api::V1::BaseController
 	def index 
 		@users = User.all
 	end
+
+	def create
+		@user = User.create(user_params)
+	end
+
   def show
     @user = User.find(params[:id])
 
     # 原文使用 Api::V1::UserSerializer
     # 我们现在使用 app/views/api/v1/users/show.json.jbuilder
     # render(json: Api::V1::UserSerializer.new(user).to_json)
+  end
+
+  private
+  def user_params
+  	params.permit(:name,:email)
   end
 end
