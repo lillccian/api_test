a144b262b62e09c3d11a3a806535fe390da80310
lillccian <lillccian@gmail.com>
2016-12-13 22:53:39 +0800

    open cors

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BaseController < ApplicationController
 	skip_before_action :verify_authenticity_token
+  before_action :enable_cors
   # disable the CSRF token
   protect_from_forgery with: :null_session
 
@@ -11,5 +12,17 @@ class Api::V1::BaseController < ApplicationController
 
   def destroy_session
     request.session_options[:skip] = true
+  end
+
+
+  
+  private
+
+  def enable_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Atmosphere-tracking-id, X-Atmosphere-Framework, X-Cache-Date, Content-Type, X-Atmosphere-Transport, X-Remote, api_key, auth_token, *'
+    response.headers['Access-Control-Request-Method'] = 'GET, POST, PUT, DELETE'
+    response.headers['Access-Control-Request-Headers'] = 'Origin, X-Atmosphere-tracking-id, X-Atmosphere-Framework, X-Cache-Date, Content-Type, X-Atmosphere-Transport,  X-Remote, api_key, *'
   end
 end
