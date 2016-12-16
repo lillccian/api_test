0e106a67cdb924e0a6d90cbfa34c89ce92754007
lillccian <lillccian@gmail.com>
2016-12-16 14:20:40 +0800

    add api crud

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,27 @@
 class Api::V1::UsersController < Api::V1::BaseController
+
+  before_action :set_user ,:only => [:show,:update,:destroy]
+
 	def index 
 		@users = User.all
 	end
-
 	def create
 		@user = User.create(user_params)
+    render json: {:message =>"add"}
 	end
-
-  def show
-    @user = User.find(params[:id])
-
-    # 原文使用 Api::V1::UserSerializer
-    # 我们现在使用 app/views/api/v1/users/show.json.jbuilder
-    # render(json: Api::V1::UserSerializer.new(user).to_json)
+  def update
+    @user = User.update(user_params)
+    render json: {:message =>"edit"}
   end
-
+  def destroy
+    @user.destroy
+    render json: {:message =>"delete"}
+  end
   private
   def user_params
   	params.permit(:name,:email)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
